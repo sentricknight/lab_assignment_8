b9f769aeef9372bd7194a0669995121465522692
sentricknight <64561889+sentricknight@users.noreply.github.com>
2023-04-09 17:30:53 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -8,13 +8,94 @@ int extraMemoryAllocated;
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+    int i, j, temp;
+    int heapSize = n;
+    extraMemoryAllocated = sizeof(int) * n;
+ 
+    for (i = (heapSize - 1) / 2; i >= 0; --i) {
+        int k = i;
+        int v = arr[k];
+        int heap = 0;
+ 
+        while (!heap && 2 * k + 1 < heapSize) {
+            int j = 2 * k + 1;
+            if (j < heapSize - 1 && arr[j] < arr[j + 1])
+                ++j;
+            if (v >= arr[j])
+                heap = 1;
+            else {
+                arr[k] = arr[j];
+                k = j;
+            }
+        }
+        arr[k] = v;
+    }
+ 
+    for (j = n - 1; j > 0; --j) {
+        temp = arr[0];
+        arr[0] = arr[j];
+        arr[j] = temp;
+        extraMemoryAllocated += sizeof(int);
+ 
+        int k = 0;
+        int v = arr[k];
+        int heap = 0;
+ 
+        while (!heap && 2 * k + 1 < j) {
+            int child = 2 * k + 1;
+            if (child < j - 1 && arr[child] < arr[child + 1])
+                ++child;
+            if (v >= arr[child])
+                heap = 1;
+            else {
+                arr[k] = arr[child];
+                k = child;
+            }
+        }
+        arr[k] = v;
+    }
 }
-
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+        int temp[r - l + 1];
+        int i, j, k;
+        i = l;
+        j = m + 1;
+        k = 0;
+        
+        while (i <= m && j <= r) {
+            if (pData[i] <= pData[j]) {
+                temp[k] = pData[i];
+                i++;
+            }
+            else {
+                temp[k] = pData[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= m) {
+            temp[k] = pData[i];
+            i++;
+            k++;
+        }
+        while (j <= r) {
+            temp[k] = pData[j];
+            j++;
+            k++;
+        }
+        for (i = l, k = 0; i <= r; i++, k++) {
+            pData[i] = temp[k];
+        }
+        extraMemoryAllocated += (r - l + 1) * sizeof(int);
+    }
 }
 
 // parses input file to an integer array
